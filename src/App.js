51345e5c05bd88552a59af627e7893fb1646ddb7
Raghav <raghav14082@iiitd.ac.in>
2018-09-22 14:06:19 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.onHandleChange = this.onHandleChange.bind(this);
+  }
+
+  onHandleChange(event){
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
